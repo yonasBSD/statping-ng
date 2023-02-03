62cc304bfee5c01b52f12a8af7220d800039845b
Bret Davis <bret.davis@intel.com>
2023-02-03 21:53:24 +0000

    Add Notification Icon to status page for service announcements
    Adds a calendar icon next to service name on status
    page to indicate number of announcements currently
    active for that service.

---
+++ frontend/src/components/Index/MessagesIcon.vue
@@ -0,0 +1,28 @@
+<template>
+    <font-awesome-layers v-if="activeMessages(messages) > 0">
+        <font-awesome-icon icon="calendar" style="color:dodgerblue"/>
+        <font-awesome-layers-text class="icon-text" :value="activeMessages(messages)" />
+    </font-awesome-layers>
+</template>
+
+<script>
+export default {
+  name: 'MessagesIcon',
+    props: {
+        messages: {
+            type: Object,
+            required: true,
+        }
+    },
+    methods: {
+        activeMessages(msgs) {
+            return msgs.filter(m => this.isAfter(this.now(), m.start_on) && this.isBefore(this.now(), m.end_on)).length
+        }
+    }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
